072a79ca2d730ad18430c3fa2f80226d5113b89c
Jim Bulkowski <jim.b@paperelectron.com>
2016-07-22 03:01:17 -0400

    add wrapper

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project Contingency
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+
+module.exports = require('./lib/Contingency')
